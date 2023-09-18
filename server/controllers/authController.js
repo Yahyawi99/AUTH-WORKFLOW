3a5f742717086f17b0c46404c3f024a68287dbb9
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-18 21:05:29 +0100

    verify email controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -72,8 +72,32 @@ const logout = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "user logged out!" });
 };
 
+// Verify email
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError("Verification Failed!");
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = "";
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Email verified." });
+};
+
 module.exports = {
   register,
   login,
   logout,
+  verifyEmail,
 };
