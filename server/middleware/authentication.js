7cacaa07f087a8dd1a964496a904226dff5405ee
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-19 10:33:00 +0100

    refacto auth middleware

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,5 +1,7 @@
 const CustomError = require("../errors");
+const Token = require("../models/Token");
 const { isTokenValid } = require("../utils");
+const { attachCookiesToResponse } = require("../utils");
 
 const authenticateUser = async (req, res, next) => {
   const { access_token, refresh_token } = req.signedCookies;
@@ -10,6 +12,26 @@ const authenticateUser = async (req, res, next) => {
       req.user = payload.user;
       return next();
     }
+
+    const payload = isTokenValid(refresh_token);
+
+    const existingToken = await Token.findOne({
+      user: payload.user.userId,
+      refreshToken: payload.refreshToken,
+    });
+
+    if (!existingToken || !existingToken?.isValid) {
+      throw new CustomError.UnauthenticatedError("Authentication Invalid");
+    }
+
+    attachCookiesToResponse({
+      res,
+      user: payload.user,
+      refreshToken: existingToken.refreshToken,
+    });
+
+    re.user = payload.user;
+    next();
   } catch (error) {
     throw new CustomError.UnauthenticatedError("Authentication Invalid");
   }
