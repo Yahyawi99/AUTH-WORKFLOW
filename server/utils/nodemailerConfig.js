a12f8cbb8b1b3dfcb4cfe0fc44f94611acf40815
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-19 09:41:30 +0100

    create token model

--- server/utils/nodemailerConfig.js
+++ server/utils/nodemailerConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   host: "smtp.ethereal.email",
   port: 587,
   auth: {
-    user: "nya.moore@ethereal.email",
-    pass: "6gaXZZFfB2UeezPNtB",
+    user: "maia18@ethereal.email",
+    pass: "tKGCKNE6hbKYAkBq9A",
   },
 };
